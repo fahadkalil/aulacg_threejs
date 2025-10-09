d5b1abc93a26f86c52e1ce8959813496c555f92c
Fahad Kalil <4945023+fahadkalil@users.noreply.github.com>
2025-10-08 21:38:01 -0300

    first example

--- examples/cube.js
+++ examples/cube.js
@@ -1,32 +1,35 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const width = window.innerWidth, height = window.innerHeight;
 
-// init
-
-const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+// propriedades da camera: https://threejs.org/manual/examples/cameras-perspective.html
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
 camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2); // https://threejs.org/docs/#api/en/geometries/BoxGeometry
+const material = new THREE.MeshNormalMaterial(); // https://threejs.org/docs/#api/en/materials/Material
+material.wireframe = false;
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( width, height );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// Permite controle da camera com mouse
+new OrbitControls(camera, renderer.domElement)
 
 // animation
 
-function animate( time ) {
+function animate(time) {
 
 	mesh.rotation.x = time / 2000;
 	mesh.rotation.y = time / 1000;
 
-	renderer.render( scene, camera );
-
+	renderer.render(scene, camera);
 }
